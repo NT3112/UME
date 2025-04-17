5028ced9bfb422d965dc12b61d876380240ec486
NT3112 <nehatendulkar3112@gmail.com>
2025-04-17 16:39:58 +0530

    Changes

--- task/app/actions.py
+++ task/app/actions.py
@@ -1,7 +1,8 @@
 
 ACTIONS = {
     "ORDER_FOOD": "Order food online",
-    "FIND_RECIPE": "Find multiple recipes",
+    
+    "FIND_REST":"Find the needed restaurants",
     "ASK_HELP": "Ask for help",
     "SET_REMINDER":"Open Clock App",
     "SET_TIME":"Set an alarm for the required time",
@@ -15,15 +16,23 @@ def suggest_actions(intent):
 
     if intent == "ORDER_FOOD":
         suggestions.append({"action": intent, "display": ACTIONS[intent]})
-        suggestions.append({"action": intent, "display": ACTIONS["FIND_RECIPE"]})
+        suggestions.append({"action": intent, "display": "Provide the necessary food options"})
+        suggestions.append({"action": intent, "display": "Get the required list of restaurants"})
+
     elif intent == "ASK_HELP":
-        suggestions.append({"action": intent, "display": ACTIONS["ASK_HELP"]})
+        suggestions.append({"action": intent, "display": ACTIONS[intent]})
+        suggestions.append({"action": intent, "display": "What kind of help"})
+        suggestions.append({"action": intent, "display": "Provide the necessary help"})
         
     elif intent=="SET_REMINDER":
         suggestions.append({"action": intent, "display": ACTIONS[intent]})
-        suggestions.append({"action": intent, "display": ACTIONS["SET_TIME"]})
+        suggestions.append({"action": intent, "display": "Set an alarm for the required time"})
+        suggestions.append({"action": intent, "display": "Restart on snoozebutton"})
     elif intent=="SHARE_NEWS":
-        suggestions.append({"action": "SHARE_NEWS", "display": ACTIONS["SHARE_NEWS"]})
+        suggestions.append({"action": intent, "display": ACTIONS[intent]})
+        suggestions.append({"action": intent, "display": "Type of  news to be shown "})
+        suggestions.append({"action": "SHARE_NEWS", "display": "Category for each news"})
+
     else:
         suggestions.append({"action": intent, "display": "Vague Request"})
 
